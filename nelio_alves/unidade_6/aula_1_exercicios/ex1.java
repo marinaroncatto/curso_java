8f21a32bfe94fd28e64ed843bc97776280248043
marinaroncatto <marinam.roncatto@gmail.com>
2025-01-23 20:49:49 -0300

    exercício while

---
+++ nelio_alves/unidade_6/aula_1_exercicios/ex1.java
@@ -0,0 +1,33 @@
+package aula_1_exercicios;
+
+import java.util.Scanner;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		/*
+		 * Escreva um programa que repita a leitura de 
+		 * uma senha até que ela seja válida. Para cada 
+		 * leitura de senha incorreta informada, 
+		 * escrever a mensagem "Senha Invalida". 
+		 * Quando a senha for informada corretamente 
+		 * deve ser impressa a mensagem 
+		 * "Acesso Permitido" e o algoritmo encerrado. 
+		 * Considere que a senha correta é o valor 2002.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int senha = sc.nextInt();
+		
+		while(senha != 2002) {
+			System.out.println("Senha Invalida");
+			senha = sc.nextInt();			
+		}
+		
+		System.out.println("Acesso Permitido");
+		
+		sc.close();
+	}
+
+}
