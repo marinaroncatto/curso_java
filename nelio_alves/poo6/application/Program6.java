29789a69b5897763b6830ec3939559ee6ddb06d5
marinaroncatto <marinam.roncatto@gmail.com>
2025-02-06 16:32:46 -0300

    Estudos Java 06/02

--- nelio_alves/poo6/application/Program6.java
+++ nelio_alves/poo6/application/Program6.java
@@ -5,43 +5,39 @@ import java.util.Scanner;
 
 import entities.Product2;
 
-public class Program2 {
+public class Program6 {
 
 	public static void main(String[] args) {
-		// POO 2
-		
+		// criando construtor
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product2 product = new Product2();
+		
 		System.out.println("Enter product data: ");
-		System.out.println("Name: ");
-		product.name = sc.nextLine();
-		System.out.println("Price: ");
-		product.price = sc.nextDouble();
-		System.out.println("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		//System.out.print("Quantity in stock: ");
+		//int quantity = sc.nextInt();
+		
+		Product2 product = new Product2(name, price);
 		
 		System.out.println();
-		System.out.println("Product data: "+product);				
-		
+		System.out.println("Product data: " + product);
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
 		int quantity = sc.nextInt();
 		product.addProducts(quantity);
-		
 		System.out.println();
-		System.out.println("Updated data: "+product);
-		
+		System.out.println("Updated data: " + product);
 		System.out.println();
 		System.out.print("Enter the number of products to be removed from stock: ");
 		quantity = sc.nextInt();
 		product.removeProducts(quantity);
-		
 		System.out.println();
-		System.out.println("Updated data: "+product);
-		
-		sc.close();		
+		System.out.println("Updated data: " + product);
+		sc.close();
 	}
 
 }
---
+++ nelio_alves/poo7/application/Program7.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entitie.Product3;
+
+public class Program7 {
+
+	public static void main(String[] args) {
+		// encapsulamento
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		//System.out.print("Quantity in stock: ");
+		//int quantity = sc.nextInt();
+		
+		Product3 product = new Product3(name, price);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated Price" + product.getPrice());
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		sc.close();
+	}
+
+}
