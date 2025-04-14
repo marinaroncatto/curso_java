2a01e23652e38c092328c8bc4031763a5c524846
marinaroncatto <marinam.roncatto@gmail.com>
2025-04-14 14:49:37 -0300

    resolução do problema

---
+++ secao14_tratamento_excecoes/exercicio/entities/exceptions/AccountException.java
@@ -0,0 +1,10 @@
+package entities.exceptions;
+
+public class AccountException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public AccountException (String msg) {
+		super(msg);
+	}
+}
