ddfe6a9eead470c8f6c5e3af618bdbffdc042234
marinaroncatto <marinam.roncatto@gmail.com>
2025-04-17 16:14:23 -0300

    Aula1 p.2
    resolvendo o exercício sem interface

---
+++ secao16_interfaces/aula1_interfaces/model/services/RentalService.java
@@ -0,0 +1,41 @@
+package model.services;
+
+import java.time.Duration;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private BrasilTaxService taxService;
+
+	public RentalService(Double pricePerHour, Double pricePerDay, BrasilTaxService taxService) {		
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+	double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes(); //calcula o tempo entre duas horas
+	double hours = minutes / 60.0;// 1h = 60 minutos
+	
+	double basicPayment;
+	if(hours <= 12.0) {
+		basicPayment = pricePerHour * Math.ceil(hours); //math.ceil arredonda pra cima		
+	}
+	else {
+		basicPayment = pricePerDay * Math.ceil(hours /24); // 1 dia = 24h
+	}
+	
+	double tax = taxService.tax(basicPayment);
+	
+	carRental.setInvoice(new Invoice(basicPayment, tax));
+	
+	}//fim função
+	
+	
+}
