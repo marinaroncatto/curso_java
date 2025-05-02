2cc15e781d069003c0f07efb5c9aeece5aabe026
marinaroncatto <marinam.roncatto@gmail.com>
2025-05-02 19:37:19 -0300

    Lambda 3
    Comparator expressão lambda "direto no argumento". Sem necessidade de instanciar.

--- secao18_programacaoFuncional_expressoesLambda/aula1_comparator/application/Program.java
+++ secao18_programacaoFuncional_expressoesLambda/aula1_comparator/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import entitites.Product;
@@ -9,16 +8,14 @@ import entitites.Product;
 public class Program {
 
 	public static void main(String[] args) {
-		// Usando método comparator v.2 Classe anônima
+		// Usando método comparator v.5 Lambda direto no argumento
 		List<Product> list = new ArrayList<>();
 
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
-
-		Comparator <Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		
-		list.sort(comp);
+			
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
 		for (Product p : list) {
 			System.out.println(p);
