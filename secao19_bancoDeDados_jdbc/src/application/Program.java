4ed831db3250b3113ef2d6c204f2bbfb5a0b0a28
marinaroncatto <marinam.roncatto@gmail.com>
2025-05-19 11:59:47 -0300

    Inserção simples com preparedStatement

--- secao19_bancoDeDados_jdbc/src/application/Program.java
+++ secao19_bancoDeDados_jdbc/src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -12,6 +15,8 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		/*
+		//parte 1 - consultando dados do banco 
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
@@ -32,8 +37,39 @@ public class Program {
 			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 			DB.closeConnection();
-		}
+		}*/
 
-	}
+		//parte 2 - inserindo dados no banco
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
+		
+		Connection conn2 = null;
+		PreparedStatement pst = null;
+		
+		try {
+			conn2 = DB.getConnection();
+			pst = conn2.prepareStatement(
+					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)"
+					+ "VALUES (?, ?, ?, ?, ?)"
+					);
+			
+			pst.setString(1, "Carl Purple");
+			pst.setString(2, "carl@gmai.com");
+			pst.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			pst.setDouble(4, 3000.0);
+			pst.setInt(5, 4);
+			
+			int rowsAffected = pst.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}catch(ParseException e) {
+			e.printStackTrace();
+		}finally {
+			DB.closeStatement(pst);
+			DB.closeConnection();
+		}
+		
+	}//main
 
 }
