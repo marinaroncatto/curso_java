fa0cadb76c74f65c6d25aa5349b8dd7b030c2c82
marinaroncatto <marinam.roncatto@gmail.com>
2025-05-20 10:26:03 -0300

    Update - atualizando dados

--- secao19_bancoDeDados_jdbc/src/application/Program.java
+++ secao19_bancoDeDados_jdbc/src/application/Program.java
@@ -2,11 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -39,6 +35,7 @@ public class Program {
 			DB.closeConnection();
 		}*/
 
+	/*
 		//parte 2 - inserindo dados no banco
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
 		
@@ -86,7 +83,35 @@ public class Program {
 		}finally {
 			DB.closeStatement(pst);
 			DB.closeConnection();
+		} */
+		
+		//parte 3: update de dados 
+		
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"UPDATE seller SET BaseSalary = BaseSalary + ? "
+					+ "WHERE (DepartmentId = ?)");
+			
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+			
+			int rowsAffected = st.executeUpdate();
+			System.out.println("Done! Rows affected: " + rowsAffected);
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+			
+		}finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
 		}
+		
+		
 		
 	}//main
 
